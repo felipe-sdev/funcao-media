adfa8e84912b43a9168f22224a00b0b3951dd288
Felipe Santos <s3t.felipe@gmail.com>
2025-11-22 14:40:01 -0300

    Criada a função média em phyton
    A função média foi criada recebendo valores , somando valores e dividindo valores

---
+++ Média-em-phyton/média.py
@@ -0,0 +1,9 @@
+print("Seja bem vindo ao programa de média de aluno")
+
+nota1= float (input("Digite a nota do aluno: "))
+nota2= float (input("Digite a segunda nota do aluno "))
+nota3= float (input("Digite a terceira nota do aluno "))
+
+média= (nota1 + nota2 + nota3)/3
+
+print("A média do aluno é: ",média)
